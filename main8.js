f3e27fba1debd6952de491d205d1f0d26c73f391
Amandi644 <okukugeorgi@yahoo.com>
2025-07-02 23:51:29 +0200

    Add files via upload

---
+++ main8.js
@@ -0,0 +1,86 @@
+
+ 
+const button = document.getElementById("demo");
+
+
+function amanDi(cost, rentax = 0.1) {
+console.log(cost * rentax , 0.1);
+console.log(`We are wining the war`);
+alert("This is Onu Palm Mill");
+}
+amanDi(2000, 0.2);
+amanDi(5000);
+amanDi();
+amanDi();
+
+function ifeanyI() {
+window.alert("The Onu oil Mill will start producing from 30th of August 2025 at Umucheke Umobom")  
+}
+ifeanyI();
+
+ 
+
+ function chisimDi(){
+  console.log('This is the best place to do your businees ');
+  }
+ chisimDi()
+
+ function lieFern() {
+  window.alert('This is a Palm seed and a poytry is 1500 Naira only');
+  console.log('He has a Palm mill');
+ }
+ 
+lieFern();
+
+const value1 ={
+product: 'sick',
+slizing: 'quality',
+verlassen: 'remedy'
+}
+
+console.log(value1.product);
+console.log(value1.slizing);
+console.log(value1.verlassen);
+
+ 
+ const value2 = {
+product1: 'rice',
+bulcket: 'water',
+cigarete: 'smock',
+ }
+console.log(value2.product1)
+console.log(value2.cigarete)
+console.log(value2.bulcket)
+
+value2 . product1 = 'mangos three';
+console.log(value2);
+
+value2.newproperty = true;
+console.log(value2);
+
+delete value2.newproperty;
+console.log(value2);
+
+let img = document.getElementById("img");
+
+function myfunction (){
+alert("alert Onu Fabrik")
+console.log("one poltry is 1.500 naira and the price for one drown is",1500 * 4);
+
+
+}
+myfunction();
+
+function lasse(){
+console.log("in umoubom is our sit of!");
+
+}
+lasse();
+lasse();
+
+
+
+let imgaTos = 2;
+
+
+ 
